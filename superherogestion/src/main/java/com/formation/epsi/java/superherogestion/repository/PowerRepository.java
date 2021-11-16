30fb5e4021f8c5d126fe03ba4d273e602e754ddb
LordTenebra <btermeau@gmail.com>
2021-11-16 14:46:28 +0100

    add .mvn

--- superherogestion/src/main/java/com/formation/epsi/java/superherogestion/repository/PowerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.formation.epsi.java.superherogestion.repository;
-
-import com.formation.epsi.java.superherogestion.model.Power;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PowerRepository extends JpaRepository<Power, Long> {
-}
--- superherogestion/src/main/java/com/formation/epsi/java/superherogestion/repository/SuperHeroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.formation.epsi.java.superherogestion.repository;
-
-import com.formation.epsi.java.superherogestion.model.SuperHero;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface SuperHeroRepository extends JpaRepository<SuperHero, Long> {
-}
